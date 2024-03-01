e196beb628170c19414b28380337e58f92b750fe
bgoncharuck <dariydragon26@gmail.com>
2024-03-01 11:28:09 +0200

    chore: async and some fields in .env

--- lib/config/app/init.dart
+++ lib/config/app/init.dart
@@ -4,6 +4,11 @@ import 'package:ass_downloader_example/use_case/i_use_case.dart';
 class InitializeApp with IUseCase<void, void> {
   @override
   Future<void> execute({void params}) async {
-    InitializeEnvironment().execute(params: []);
+    await InitializeEnvironment().execute(
+      params: [
+        'DOMAIN_URL',
+        'SENTRY_DSN',
+      ],
+    );
   }
 }
