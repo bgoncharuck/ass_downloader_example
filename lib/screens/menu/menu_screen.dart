7773d3a9a54d04e0331d30406e67d4c709cbe0da
bgoncharuck <dariydragon26@gmail.com>
2024-03-07 15:37:04 +0200

    feat: select asset groups in menu

--- lib/screens/menu/menu_screen.dart
+++ lib/screens/menu/menu_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ass_downloader_example/models/asset_groups/asian_animals/asian_animals.dart';
 import 'package:ass_downloader_example/screens/menu/menu_screen_controller.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -17,17 +18,27 @@ class _MenuScreenState extends State<MenuScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final screenController = MenuScreenLocator.of(context);
+
     return PopScope(
       canPop: false,
       child: SafeArea(
         child: CupertinoPageScaffold(
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CupertinoButton(
-                  child: const Text('Animals'),
-                  onPressed: () => Navigator.pushNamed(context, '/animals'),
+            child: CustomScrollView(
+              physics: const NeverScrollableScrollPhysics(),
+              slivers: [
+                SliverList.builder(
+                  itemBuilder: (_, index) {
+                    final groupKey =
+                        screenController.assetGroups.keys.elementAt(index);
+
+                    return CupertinoButton(
+                      child: Text(groupKey),
+                      onPressed: () =>
+                          screenController.selectAssetGroups(context, groupKey),
+                    );
+                  },
                 ),
               ],
             ),
--- lib/screens/menu/menu_screen_controller.dart
+++ lib/screens/menu/menu_screen_controller.dart
@@ -1,13 +1,26 @@
+import 'package:ass_downloader_example/app/routes.dart';
 import 'package:ass_downloader_example/controllers/screen_controller.dart';
+import 'package:ass_downloader_example/models/asset_groups/asian_animals/asian_animals.dart';
 import 'package:ass_downloader_example/use_case/presentation/remove_native_splash.dart';
 import 'package:flutter/widgets.dart';
 
 class MenuScreenController extends ScreenController {
+  final Map<String, List<String>> assetGroups = {
+    'Asian Animals': asianAnimals.keys.toList(),
+  };
+
   void init() {
     if (super.initOnce) {
       return;
     }
     const RemoveNativeSplash().execute();
+  }
+
+  void selectAssetGroups(BuildContext context, String assetGroupName) {
+    Navigator.of(context).pushNamed(
+      pathGroups,
+      arguments: assetGroups[assetGroupName],
+    );
   }
 }
 
