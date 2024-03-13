14fb539f89e75d4e3ef92610eb43f70c9440c069
bgoncharuck <dariydragon26@gmail.com>
2024-03-13 15:21:27 +0200

    docs: crash watcher

--- lib/config/app/routines/crash_watcher.dart
+++ lib/config/app/routines/crash_watcher.dart
@@ -5,6 +5,7 @@ import 'package:sentry_flutter/sentry_flutter.dart';
 class SetCrashWatcherOverAppRunner with IUseCase<void Function(), void> {
   const SetCrashWatcherOverAppRunner();
 
+  /// params: A function to be executed as the main application runner.
   @override
   Future<void> execute({required void Function() params}) async {
     await SentryFlutter.init(
@@ -16,9 +17,11 @@ class SetCrashWatcherOverAppRunner with IUseCase<void Function(), void> {
 
 void _sentryOptions(SentryFlutterOptions options) {
   options
-    ..dsn = env['SENTRY_DSN']
-    ..tracesSampleRate = 1.0
-    ..sendDefaultPii = true
-    ..attachScreenshot = false
-    ..attachStacktrace = true;
+    ..dsn = env['SENTRY_DSN'] // Set the Sentry DSN from environment variables.
+    ..tracesSampleRate =
+        1.0 // Set the sampling rate for performance traces to 100%.
+    ..sendDefaultPii =
+        true // Enable sending default personally identifiable information.
+    ..attachScreenshot = false // Disable attaching screenshots to events.
+    ..attachStacktrace = true; // Enable attaching stack traces to events.
 }
--- lib/services/logger/plugins/multiple_libs_logging.dart
+++ lib/services/logger/plugins/multiple_libs_logging.dart
@@ -3,12 +3,19 @@ import 'package:ass_downloader_example/services/logger/plugins/debug_print.dart'
 import 'package:ass_downloader_example/services/logger/plugins/sentry_flutter.dart';
 import 'package:flutter/foundation.dart';
 
+/// A logging implementation that aggregates logging from multiple logging libraries.
 class MultipleLibrariesLogging implements LoggingLibrary {
+  /// The list of logging libraries to be used.
   final Iterable<LoggingLibrary> libraries = [
     const SentryLogging(),
+    // Initialize DebugPrintLogging for logging messages during debug mode.
     if (kDebugMode) const DebugPrintLogging(),
   ];
 
+  /// Logs an exception to all configured logging libraries.
+  ///
+  /// This method propagates the given [e] (exception) and [t] (stack trace)
+  /// to all logging libraries registered in [libraries].
   @override
   Future<void> exception(Object e, StackTrace t) async {
     for (final lib in libraries) {
@@ -16,6 +23,10 @@ class MultipleLibrariesLogging implements LoggingLibrary {
     }
   }
 
+  /// Logs a message to all configured logging libraries.
+  ///
+  /// This method propagates the given [message] to all logging libraries
+  /// registered in [libraries].
   @override
   Future<void> message(String message) async {
     for (final lib in libraries) {
