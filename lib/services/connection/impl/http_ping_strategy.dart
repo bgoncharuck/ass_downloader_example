d72ff5b9931cf835829fd0ba9eb43210ada3e375
bgoncharuck <dariydragon26@gmail.com>
2024-03-13 15:23:24 +0200

    docs: connection checker

--- lib/services/connection/impl/http_ping_strategy.dart
+++ lib/services/connection/impl/http_ping_strategy.dart
@@ -6,6 +6,8 @@ import 'package:http/http.dart' as http;
 class HttpPingStrategy implements ConnectionCheckerStrategy {
   const HttpPingStrategy();
 
+  /// Returns a list of URLs that are reachable.
+  /// Returns an empty list if none of the URLs are reachable.
   @override
   Future<List<String>> isConnected(List<String> urls) async {
     final futures = <Future<String>>[];
@@ -23,6 +25,8 @@ class HttpPingStrategy implements ConnectionCheckerStrategy {
     return availableDomains.where((domain) => domain.isNotEmpty).toList();
   }
 
+  /// Returns the URL if the response status code is 200 (OK),
+  /// otherwise returns an empty string
   Future<String> _notPing(String url) async {
     final response = await http.get(Uri.parse(url));
     return response.statusCode == 200 ? url : '';
